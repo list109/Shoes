666e5c1c8895d6923fc03846e89cd52da0f01903
list109 <git_109@list.ru>
2020-05-07 19:36:18 +0300

    fix html, css

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,9 @@
 //  menu =================================
 menu(menuBtn, 'btn_focused', {elements: [mobileMenu], classList: ["menu_focused"]});
 
+
+
 // slider =================================
-
-
-
 new Slider(slider, {pointClassName: 'goods-list__item_focused', pointsElem: sliderPoints, time: 2});
+
 
