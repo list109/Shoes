315706491d382e52b24c13cb8592951fc90c0169
list109 <git_109@list.ru>
2020-05-08 11:31:03 +0300

    Update main.js

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,8 @@ menu(menuBtn, 'btn_focused', {elements: [mobileMenu], classList: ["menu_focused"
 
 
 // slider =================================
-new Slider(slider, {pointClassName: 'goods-list__item_focused', pointsElem: sliderPoints, time: 2});
+window.addEventListener('load', () => {
+    new Slider(slider, {pointClassName: 'goods-list__item_focused', pointsElem: sliderPoints, time: 2});
+});
 
 
